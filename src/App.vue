d538b972415db416bb367c91b4c8448a39007f1c
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-15 13:25:57 +0100

    milestone 0 : completed

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
-<!-- https://api.themoviedb.org/3 -->
 <template>
-  <div>
-
-  </div>
+  <main>
+    <section id="movies" class="container">
+      <h2>Movies</h2>
+      <div class="row">
+        <div class="col-12 col-md-4 col-xl-3" v-for="(movie, index) in store.moviesList" :key="movie.id">
+          <!-- qua componente card -->
+          {{ movie.title }}
+        </div>
+      </div>
+    </section>
+    <section id="series" class="container">
+      <h2>TV Series</h2>
+      <div class="row">
+        <div class="col-12 col-md-4 col-xl-3" v-for="(serie, index) in store.seriesList" :key="serie.id">
+          <!-- qua componente card -->
+          {{ serie.name }}
+          <!-- basisc images (configuration) -->
+          <img src="" alt="">
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from './assets/data/store.js'
 export default {
   name: "App",
   data() {
     return {
-
+      store,
     }
   },
   methods: {
-
+    getMoviesAndSeries() {
+      const movieUrl = store.apiUrl + store.endpoint.movies
+      axios.get(movieUrl, { params: store.params }).then((res) => {
+        console.log(res.data.results);
+        store.moviesList = res.data.results
+      })
+      const seriesUrl = store.apiUrl + store.endpoint.series
+      axios.get(seriesUrl, { params: store.params }).then((res) => {
+        console.log(res.data.results);
+        store.seriesList = res.data.results
+      })
+    },
   },
   created() {
-
+    this.getMoviesAndSeries()
   }
 }
 </script>
--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -1,3 +1,19 @@
 import { reactive } from "vue";
 
-export const store = reactive({});
+export const store = reactive({
+  apiUrl: "https://api.themoviedb.org/3/",
+  endpoint: {
+    movies: "search/movie",
+    series: "search/tv",
+  },
+  params: {
+    query: "null",
+    api_key: "2f40b8e5f9dc4424458c6502f4a7ce56",
+  },
+  error: "error",
+  loading: false,
+  moviesList: [],
+  seriesList: [],
+});
+
+// https://image.tmdb.org/t/p/w342
