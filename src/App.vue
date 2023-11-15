90813ddddbe8e318ebcf5e3115a510cc528782b6
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-15 12:21:08 +0100

    scaffholding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!-- https://api.themoviedb.org/3 -->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
